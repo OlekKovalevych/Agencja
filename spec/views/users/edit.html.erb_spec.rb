89d4d9d40e091878d1cffc3f17dd03bd948c4558
OlekKovalevych <kovalevich.148@gmail.com>
2022-08-24 22:26:36 +0200

    used rubocop

--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -1,21 +1,22 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "users/edit", type: :view do
+RSpec.describe 'users/edit', type: :view do
   before(:each) do
     @user = assign(:user, User.create!(
-      first_name: "MyString",
-      last_name: "MyString"
-    ))
+                            first_name: 'MyString',
+                            last_name: 'MyString'
+                          ))
   end
 
-  it "renders the edit user form" do
+  it 'renders the edit user form' do
     render
 
-    assert_select "form[action=?][method=?]", user_path(@user), "post" do
+    assert_select 'form[action=?][method=?]', user_path(@user), 'post' do
+      assert_select 'input[name=?]', 'user[first_name]'
 
-      assert_select "input[name=?]", "user[first_name]"
-
-      assert_select "input[name=?]", "user[last_name]"
+      assert_select 'input[name=?]', 'user[last_name]'
     end
   end
 end
--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "users/show", type: :view do
+RSpec.describe 'users/show', type: :view do
   before(:each) do
     @user = assign(:user, User.create!(
-      first_name: "First Name",
-      last_name: "Last Name"
-    ))
+                            first_name: 'First Name',
+                            last_name: 'Last Name'
+                          ))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     expect(rendered).to match(/First Name/)
     expect(rendered).to match(/Last Name/)
