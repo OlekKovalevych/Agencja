5887a2712b15770c3f93b68d0aeafceabe2812ca
OlekKovalevych <kovalevich.148@gmail.com>
2022-08-25 22:18:56 +0200

    set up webpack

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+  devise_for :users
+  root to: "public#404.html"
+
+  resources :users, as: 'users'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
