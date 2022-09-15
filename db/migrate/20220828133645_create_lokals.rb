4deb11ac2670a885a9b6c95749a02223e7af3f9f
OlekKovalevych <kovalevich.148@gmail.com>
2022-09-15 09:57:27 +0200

    WIP

--- db/migrate/20220828133645_create_lokals.rb
+++ db/migrate/20220828133645_create_lokals.rb
@@ -6,7 +6,7 @@ class CreateLokals < ActiveRecord::Migration[7.0]
       t.string :ulica
       t.integer :cena
       t.datetime :rezerv
-      t.datetime :end_time
+      t.datetime :end_time 
 
       t.timestamps
     end
